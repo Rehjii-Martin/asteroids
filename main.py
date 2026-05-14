5695819f8420f8ba3e09dd381fefba93380a3855
Rehjii Martin <rehjiimartin15@gmail.com>
2026-05-13 19:39:27 -0700

    Added player and rotation mechanics

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -20,8 +28,15 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
 
         pygame.display.flip()
+        clock.tick(60)
+
+        dt = clock.tick(60)
+        
+
 
 if __name__ == "__main__":
     main()
